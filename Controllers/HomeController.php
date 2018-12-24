c4c5f3d90eab8d3db4dd794dd4f695c51465dc70
Mercier Quentin <contact@mopoti.fr>
2018-12-24 17:39:38 +0100

    add font and reset css

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 //Return to movie.php for start functions who display content of home page with requests
 require('../Models/model.php');
-$top5=home();
+
 
 //Sent of home view when controller is ready
 require('../Views/home.php');
--- Views/home.php
+++ Views/home.php
@@ -5,13 +5,13 @@
   <title>CinéPédia</title>
   <meta name="author" content="Quentin Mercier">
   
-  <link rel="stylesheet" href="./public/css/main.css">
+  <link rel="stylesheet" href="./public/css/style.css">
   
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <header>
-    
+
     </header>
 </body>
 </html>
